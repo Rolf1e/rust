08cebd2002a065439c637fff9b3d592085e4da61
rolf1e <tigran.slama@laposte.net>
2020-07-15 19:06:37 +0200

    robot-name

--- exercises/robot-name/src/lib.rs
+++ exercises/robot-name/src/lib.rs
@@ -1,15 +1,42 @@
-pub struct Robot;
+use rand::Rng;
+
+pub struct Robot{
+    name : String
+}
 
 impl Robot {
     pub fn new() -> Self {
-        unimplemented!("Construct a new Robot struct.");
+        Robot { name: String::from(generate_name())}
     }
 
     pub fn name(&self) -> &str {
-        unimplemented!("Return the reference to the robot's name.");
+        println!("{}", &self.name);
+        &self.name
     }
 
     pub fn reset_name(&mut self) {
-        unimplemented!("Assign a new unique name to the robot.");
+        println!("{}", &self.name);
+        self.name = generate_name()
     }
+
+}
+
+fn generate_name() -> String { 
+    let number = generate_random_number();
+    match number {
+        0 ..=9 => format!("{}{}00{}", generate_random_chars(), generate_random_chars(), number),
+        10 ..=99 => format!("{}{}0{}", generate_random_chars(), generate_random_chars(), number),
+        _ => format!("{}{}{}", generate_random_chars(), generate_random_chars(), number),
+    }
+}
+
+
+fn generate_random_chars() -> char {
+    rand::thread_rng()
+        .gen_range(b'A', b'Z') as char
+}
+
+fn generate_random_number() -> i32 {
+    rand::thread_rng()
+        .gen_range(0, 999)
 }
