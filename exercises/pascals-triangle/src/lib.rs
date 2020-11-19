dc7d4ed828e5ad1d8f8246bb0e2efb905a41bcc8
Rolf1e <tigran.slama@laposte.net>
2020-11-19 22:49:35 +0100

    Pascal triangle

--- exercises/pascals-triangle/src/lib.rs
+++ exercises/pascals-triangle/src/lib.rs
@@ -1,11 +1,31 @@
-pub struct PascalsTriangle;
+pub struct PascalsTriangle {
+    rows: Vec<Vec<u32>>,
+}
 
 impl PascalsTriangle {
     pub fn new(row_count: u32) -> Self {
-        unimplemented!("create Pascal's triangle with {} rows", row_count);
+        PascalsTriangle {
+            rows: create_triangle(row_count),
+        }
     }
 
     pub fn rows(&self) -> Vec<Vec<u32>> {
-        unimplemented!();
+        self.rows.clone()
     }
+}
+
+fn create_triangle(row_count: u32) -> Vec<Vec<u32>> {
+    (0..row_count).map(|x| create_row(x)).collect()
+}
+
+fn create_row(row_length: u32) -> Vec<u32> {
+    let mut row: Vec<u32> = vec![1];
+
+    for i in 1..=row_length {
+        if let Some(&last) = row.last() {
+            row.push(last * (row_length + 1 - i) / i)
+        }
+    }
+
+    row
 }
